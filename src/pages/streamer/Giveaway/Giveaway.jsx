1d9dff89626759d301d33ffb58f600aa9a47a7f6
Shree Shub <shubhamashishpatel@gmail.com>
2023-02-01 01:10:58 +0530

    Added Giveawy feature

--- src/pages/streamer/Giveaway/Giveaway.jsx
+++ src/pages/streamer/Giveaway/Giveaway.jsx
@@ -5,10 +5,10 @@ import { Button, Form, FormGroup, FormControl, Spinner } from "react-bootstrap";
 import "./batchCall.css";
 import { ethers, Contract } from "ethers";
 import { abi } from "./config2";
-import * as Push from "@pushprotocol/restapi";
 import { useContext } from "react";
 import Auth from "../../../context/Auth";
-
+import { sendNotification } from '@pushprotocol/restapi/src/lib/payloads';
+import * as Push from "@pushprotocol/restapi";
 const Giveaway = () => {
 
     const {address} = useContext(Auth)
@@ -25,45 +25,39 @@ const Giveaway = () => {
     const [Amountt, setAmountt] = useState("");
     const [EndDate, setED] = useState("");
     const [isApproveButtonLoading, setIsApproveButtonLoading] = useState(false);
-
+    const [subs,setSubs] = useState("");
     //will be used to approve super token contract to spend DAI
-async function daiApprove(approveAmount) {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-  
-    const sf = await Framework.create({
-      chainId: 5,
-      provider: provider
-    });
-    const chainId = await window.ethereum.request({ method: "eth_chainId" });
-    const signer = provider.getSigner();
-  
-  
-    const DAI = new ethers.Contract(
-      "0x88271d333C72e51516B67f5567c728E702b3eeE8",
-      daiABI,
-      signer
-    );
-  
-    console.log(DAI);
-    try {
-      console.log("approving DAI spend");
-      await DAI.approve(
-        "0xF2d68898557cCb2Cf4C10c3Ef2B034b2a69DAD00",
-        ethers.utils.parseEther(approveAmount.toString())
-      ).then(function (tx) {
-        console.log(
-          `Congrats, you just approved your DAI spend. You can see this tx at https://goerli.etherscan.io/tx/${tx.hash}`
-        );
+    async function _sendNotification() {
+      let sender = address;
+      let reciver =subs ;
+      console.log("Rev:",reciver);
+      console.log("amt:", amt, "Receiver:", reciver, "sender:", sender);
+      const apiResponse = sendNotification({
+        signer,
+        type: 3, // target
+        identityType: 2, // direct payload
+        notification: {
+          title: 'Giveaway',
+          body: `Giveaway by ${sender}`
+        },
+        payload: {                                                                                                                             
+          title: `Giveaway Winner is `,
+          body: `Congrats ${reciver} , You have received ${amt}$`,
+          cta: '',
+          img: ''
+        },
+        channel: `eip155:5:${sender}`, // your channel address
+        env: 'staging'
       });
-    } catch (error) {
-      console.error(error);
-    }
-  }
   
+      console.log(apiResponse);
+    }
   //where the Superfluid logic takes place
-  async function executeBatchCall(upgradeAmt, recipient, flowRate) {
+  async function executeBatchCall(_msg,upgradeAmt) {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     var accounts;
+var flowRate = 500000000000000;
+ console.log("Subs is :",await _msg);
     if(window.ethereum){
       try {
            accounts = await window.ethereum.request({
@@ -93,7 +87,7 @@ async function daiApprove(approveAmount) {
       //upgrade and create stream at once
       const createFlowOperation = DAIx.createFlow({
         sender: accounts[0],
-        receiver: recipient,
+        receiver: _msg,
         flowRate: flowRate
       });
   
@@ -107,15 +101,16 @@ async function daiApprove(approveAmount) {
             `Congrats - you've just successfully executed a batch call!
             You have completed 2 operations in a single tx 🤯
             View the tx here:  https://goerli.etherscan.io/tx/${tx.hash}
-            View Your Stream At: https://app.superfluid.finance/dashboard/${recipient}
+            View Your Stream At: https://app.superfluid.finance/dashboard/${subs}
             Network: Goerli
             Super Token: DAIx
             Sender: 0xDCB45e4f6762C3D7C61a00e96Fb94ADb7Cf27721
-            Receiver: ${recipient},
+            Receiver: ${subs},
             FlowRate: ${flowRate}
             `
           );
           //Add a push Notification here
+          _sendNotification();
         });
   
     } catch (error) {
@@ -125,56 +120,6 @@ async function daiApprove(approveAmount) {
       console.error(error);
     }
   }
-  
-  async function scheduleStream(_RA, _startDate, _Amt, _EndDate) {
-    handelRA()
-    console.log("Helllo");
-    var accounts;
-    if(window.ethereum){
-      try {
-           accounts = await window.ethereum.request({
-              method: "eth_requestAccounts",
-          });
-         // console.log(accounts)           
-      } catch (error) {
-          console.log(error)
-      }
-  }
-  
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const chainId = await window.ethereum.request({ method: "eth_chainId" });
-    var d1 = new Date(_startDate);
-    var d2 = new Date(_EndDate);
-    const d11 = parseInt(d1.getTime())/1000;
-    const d22 = parseInt(d2.getTime())/1000;
-    const _Amtt = parseInt(_Amt);
-    /* console.log("Start Date = "+d1.getTime());
-    console.log("End Date = "+d2.getTime()); */
-    const signer = provider.getSigner();
-    const contractAddress = "0xA6134E107FCAEAab6140CB8fb92Dbf5bd9EF6C86";
-    const contractABI = abi;
-    try{
-    const peer = new Contract(
-      contractAddress,
-      contractABI,
-      signer
-    );
-    const superToken = "0xF2d68898557cCb2Cf4C10c3Ef2B034b2a69DAD00";
-  /*   console.log(typeof(accounts),typeof(_RA), typeof(d11), typeof(d11),typeof(315777553760070), typeof(_Amtt), typeof(d22),"0x", "0x");
-    console.log((accounts),(_RA), (d11), (d11),(315777553760070), (_Amtt), (d22),"0x", "0x");
-   */
-    console.log("Writing");
-    const coffeeTxn = await peer.createFlowSchedule(superToken, _RA, d11, d11,315777553760070, _Amtt, d22, "0x", "0x");
-    console.log("Writing2");
-  
-    await coffeeTxn.wait();
-    console.log("mined", coffeeTxn.hash);
-    }catch{
-      console.log("Error");
-    }
-    console.log("Written");
-  }
-
   
     function calculateFlowRate(amount) {
       if (typeof Number(amount) !== "number" || isNaN(Number(amount)) === true) {
@@ -211,8 +156,8 @@ async function daiApprove(approveAmount) {
 
     async function getSubs(){
 
-        let meraAccount = address
-    
+        let meraAccount = address;
+
         const channelData = await Push.channels._getSubscribers({
           channel: `eip155:5:${address}`, // channel address in CAIP
           env: 'staging'
@@ -231,6 +176,9 @@ async function daiApprove(approveAmount) {
           console.log("In While");
           data = findRand();
         }
+        console.log("Random Subs are :",data);
+        setSubs(data);
+
         return data;
       }
   
@@ -314,15 +262,14 @@ async function daiApprove(approveAmount) {
         <br>
         </br>
         <form>
-          <h2>Schedule Money stream starts here,A stream will be scheduled at 12 AM GMT timezone</h2>
-          <input type="date" placeholder="Start Date" onChange={handelSD} name="StartDate" value={StartDate}></input>
+          <h2>Select a Random subscriber from your PUSH Channel and send him Love in form of Donations</h2>
           <input type="Number" placeholder="Amount" name="Amount" onChange={handelAmount} value={Amountt}></input>
-          <input type="date" placeholder="End Date" name="EndDate" onChange={handelED} value={EndDate}></input>
           <button
             type="submit"
             onClick={(e) => {
               e.preventDefault();
-              scheduleStream(receiversAddress, StartDate, Amountt, EndDate);
+              
+              executeBatchCall(getSubs(),Amountt);
             }}
           >
             Click to schedule a stream
